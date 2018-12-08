d8848ec5929958e04fd44a4be920345561030867
vlad <slavayus@gmail.com>
2018-12-08 22:05:10 +0300

    read with system call

--- xar/xar.c
+++ xar/xar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
     char *cmd = malloc(sizeof(char *));
@@ -10,22 +11,11 @@ int main(int argc, char **argv) {
         strcat(cmd, " ");
     }
 
-//    char data[BUFSIZ];
-//    ssize_t read_bytes = 0;
-//    while ((read_bytes = read(stdin, data, BUFSIZ)) > 0) {
-//        strcat(cmd, data);
-//        printf("system cmd: %s\n",cmd);
-//    }
-
-
-    char buf[BUFSIZ];
-
-    while (buf[strlen(buf) - 1] != '\n') {
-        fgets(buf, sizeof buf, stdin);
-        strcat(cmd, buf);
+    char data[BUFSIZ];
+    ssize_t read_bytes = 0;
+    while ((read_bytes = read(STDIN_FILENO, data, BUFSIZ)) > 0) {
+        strcat(cmd, data);
     }
-
-    printf("system cmd: %s\n",cmd);
 
     system(cmd);
 }
