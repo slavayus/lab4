e5409009b52697eb01c580c28e76fc826e4313c2
vlad <slavayus@gmail.com>
2018-12-08 12:00:57 +0300

    open source and destination files

--- perl/mcp.pl
+++ perl/mcp.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl -T
 use strict;
 use warnings;
+use Fcntl;
 
 my $result = check_args();
 
 if ($result) {
     exit($result)
 }
+
+my $source_file = open_source_file();
+my $filemode = (stat($source_file))[2];
+my $destination_file = open_destination_file($filemode);
+printf("Source file = %s", $source_file);
+printf("Source file = %s", $destination_file);
 
 
 sub check_args {
@@ -19,4 +26,16 @@ sub check_args {
         return 2
     }
     return 0
+}
+
+sub open_source_file {
+    sysopen(my $fh, "1", O_RDONLY)
+        or die "Can't open < source file: $!";
+    return $fh
+}
+
+sub open_destination_file {
+    sysopen(my $fh, "2", O_WRONLY | O_CREAT, $_[0])
+        or die "Can't open > destination file: $!";
+    return $fh
 }
